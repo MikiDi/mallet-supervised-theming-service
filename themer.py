4db364d7d1b518852fc56ae4728b9e73ed57eda0
Michaël Dierick <michael@dierick.io>
2017-02-07 14:40:44 +0100

    Improved error handling & logging

--- themer.py
+++ themer.py
@@ -150,9 +150,10 @@ def evaluate_theme(theme):
     themes_query = build_topicscore_query(theme)
     try:
         topicscores = helpers.query(themes_query)["results"]["bindings"]
-        helpers.log(topicscores)
+        #helpers.log(topicscores)
     except Exception as e:
         helpers.log("Querying SPARQL-endpoint failed:\n" + str(e))
+        return
     topichash = {}
     for topicscore in topicscores:
         score = float(topicscore["score"]["value"])
@@ -162,11 +163,15 @@ def evaluate_theme(theme):
         except KeyError:
             topichash[topicuri] = score
 
-    #weighing (divide by number of events assigned to a theme)
+    #weighing: divide by sum of all scores (so that sum of all topicprints = 1)
     weightedhash = {k: v/sum(topichash.values()) for k, v in topichash.items()}
 
-    helpers.update(build_topicprint_update_query(theme, weightedhash))
-    helpers.log('Calculated & inserted topic-fingerprint for theme "{}" ({} topicprints)'.format(theme, len(topichash)))
+    try:
+        helpers.update(build_topicprint_update_query(theme, weightedhash))
+        helpers.log('Calculated & inserted topic-fingerprint for theme "{}" ({} topicprints)'.format(theme, len(topichash)))
+    except Exception as e:
+        helpers.log("Querying SPARQL-endpoint failed, exiting:\n" + str(e))
+        return
 
 def multiply_dicts(x, y):
     return {k: x.get(k, 0) * y.get(k, 0) for k in set(x) & set(y)}
@@ -177,7 +182,8 @@ def run():
         results = helpers.query(build_themes_query())["results"]["bindings"]
         themes = [theme["theme"]["value"] for theme in results]
     except Exception as e:
-        helpers.log("Querying SPARQL-endpoint failed:\n" + str(e))
+        helpers.log("Querying SPARQL-endpoint failed, exiting:\n" + str(e))
+        return
 
     for theme in themes:
         try:
@@ -189,32 +195,36 @@ def run():
     # Build a dictionary of topicscores ("topicprints") per curated theme
     try:
         results = helpers.query(build_topicprint_select_query())["results"]["bindings"]
-        curatedthemes = {}
-        for result in results:
-            theme = result["theme"]["value"]
-            topic = result["topic"]["value"]
-            score = float(result["score"]["value"])
-            try:
-                curatedthemes[theme][topic] = score
-            except KeyError:
-                curatedthemes[theme] = {topic: score}
+        helpers.log('Queried topicprints for all curated themes ({} topicprints)'.format(len(results)))
     except Exception as e:
-        helpers.log("Querying SPARQL-endpoint failed:\n" + traceback.format_exc())
+        helpers.log("Querying SPARQL-endpoint failed, exiting:\n" + str(e))
+        return
+    curatedthemes = {}
+    for result in results:
+        theme = result["theme"]["value"]
+        topic = result["topic"]["value"]
+        score = float(result["score"]["value"])
+        try:
+            curatedthemes[theme][topic] = score
+        except KeyError:
+            curatedthemes[theme] = {topic: score}
 
     # Build a dictionary of topicscores per event
     try:
         results = helpers.query(build_events_query())["results"]["bindings"]
-        events = {}
-        for result in results:
-            event = result["event"]["value"]
-            topic = result["topic"]["value"]
-            score = float(result["score"]["value"])
-            try:
-                events[event][topic] = score
-            except KeyError:
-                events[event] = {topic: score}
+        helpers.log('Queried topicscores for all curated themes ({} topicscores)'.format(len(results)))
     except Exception as e:
-        helpers.log("Querying SPARQL-endpoint failed:\n" + traceback.format_exc())
+        helpers.log("Querying SPARQL-endpoint failed, exiting:\n" + str(e))
+        return
+    events = {}
+    for result in results:
+        event = result["event"]["value"]
+        topic = result["topic"]["value"]
+        score = float(result["score"]["value"])
+        try:
+            events[event][topic] = score
+        except KeyError:
+            events[event] = {topic: score}
 
     # Make a map by event of it's topicscores and weights by theme (topicprints) multiplied
     weights_by_event_by_cat = {}
@@ -226,5 +236,8 @@ def run():
     # Add learned themes to graph, event by event
     # TODO do this in previous loop without building dict for all events (less stack)
     for event, themes in weights_by_event_by_cat.items():
-        helpers.update(build_learnedthemes_update_query(event, themes))
-        helpers.log('Learned & inserted themes for event "{}" ({} themes)'.format(event, len(themes)))
+        try:
+            helpers.update(build_learnedthemes_update_query(event, themes))
+            helpers.log('Learned & inserted themes for event "{}" ({} themes)'.format(event, len(themes)))
+        except Exception as e:
+            helpers.log("Querying SPARQL-endpoint failed:\n" + str(e))
